184169e6b087773d5113d4991103f52ab712351b
gabitolage <gglage11@gmail.com>
2023-10-18 17:47:00 -0300

    update escolha de interesses

---
+++ codigo/modules/Gabriel/script.js
@@ -0,0 +1,17 @@
+function criarBotao() {
+    fetch('db.json')
+        .then(function (response) { return response.json(); })
+        .then(function (db) {
+            let textoHTML = '';
+            let index=0
+            db.interesses.map(e =>{
+                textoHTML +=
+                    `<div class="interesses">
+                        <input type="checkbox" class="btn-check" id="btncheck${index}" autocomplete="off">
+                        <label class="btn btn-outline-primary" for="btncheck${index}">${e.interesse}</label>
+                    </div>`;
+                    index++
+            })
+            document.getElementById('botoes').innerHTML = textoHTML;
+        })
+}
