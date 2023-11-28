4335a7c7f82f81b6e1480a6e1c66d8b347a25c86
gabitolage <gglage11@gmail.com>
2023-11-28 15:01:24 -0300

    Pagina Inicial

---
+++ codigo/modules/Pagina Inicial/script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    setTimeout(function() {
+        var conteudo = document.getElementById('conteudo');
+        conteudo.classList.add('mostrar');
+    }, 1000); // Espera 1 segundo (1000 milissegundos) antes de mostrar os elementos
+}
